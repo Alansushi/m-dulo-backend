5379a8f4cd298c7b99a2b9b80ad968445e47d7e9
Alan Guerrero García <age_allison@hotmail.com>
2022-06-07 22:05:14 -0500

    clase 14

--- kodemiaapi/src/index.js
+++ kodemiaapi/src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config() 
 //Imprtamos paquetes con require
 const express = require("express")
+const { get } = require("express/lib/response")
 const mongoose = require("mongoose")
 
 const Koder = require('./models/koder.model')
@@ -14,11 +15,43 @@ const URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retry
 
 const app = express()
 
+app.use(express.json());
+
+
+app.post("/koders",async(req,res)=>{
+    mongoose.connect(URL)
+    .then(async(connection)=>{
+        console.log('Estamos conectados a nuestra base datos!')
+        const newKoder = new Koder({
+            nombre: 'Iván',
+            genero: 'masculino',
+            edad: 29
+        })
+    await Koder.create(newKoder)
+        
+
+    })
+    .catch((error)=>{
+        console.log("No nos conectamos a la base de Datos")
+        console.error(error)
+    })
+})
+
 app.get("/koders",async (req,res)=>{
-    const koders = await Koder.find({});
+    const edad =  Number(req.query.edad)
+    const count = Number(req.query.count)
+
+    console.log(edad)
+
+    const query ={
+        $or: [{edad: edad}]
+    }
+    const koders = await Koder.find(query)
+    console.log(koders)
 
     res.json(koders)
 })
+
 
 mongoose
     .connect(URL)
